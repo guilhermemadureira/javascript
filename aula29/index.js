7811a9398cae376dec7b969df834b75729a6c029
Guilherme <64824790+guilhe100@users.noreply.github.com>
2020-12-17 21:22:47 -0300

    add aula29

---
+++ aula29/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores Lógicos
+&& -> and > e > todas as expressões tem que ser true para retornar o true
+ || -> OR > ou > Só retorna falso se todas tiverem falsas e se tiver um true ja é considerada como verdadeira
+ ! > not > não 
+*/
+
+//const expressao= true && true && true && true;
+//const expresso= false || false || false || true ;
+
+/* const usuario='Guilherme';
+const senha ='123456';
+
+const login= usuario === "Guilherme" && senha==='123456';
+ */
+
+
+console.log(true)
+
