7c23a122ec423950a347a202da3abfef07d9fa9e
Guilherme <64824790+guilhe100@users.noreply.github.com>
2020-12-17 21:22:26 -0300

    ADD aula28

---
+++ aula28/index.js
@@ -0,0 +1,15 @@
+/*
+operador de comparação
+>maior que
+>= maior que ou igual a
+<menor que
+<= menor que ou igual a 
+== igualdade (valor)n
+=== igualdade estrita (valor e tipo)
+!= diferente(valor) n
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = 10;
+const comp = num1 !== num2;
+console.log(comp)
