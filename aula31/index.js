052319d97a29f31c03c81749e65db368161bcffe
Guilherme <64824790+guilhe100@users.noreply.github.com>
2020-12-18 22:02:54 -0300

    ADD aula31

---
+++ aula31/index.js
@@ -0,0 +1,22 @@
+/* const hora = 23;
+
+if(hora >= 0 && hora <= 11){
+    console.log('bom dia')
+
+}
+else if(hora >= 12 && hora <= 17){
+    console.log('boa tarde')
+}
+else if(hora >= 18 && hora <= 23){
+    console.log('boa noite')
+}else{
+    console.log('oi')
+} */
+
+const grana = false;
+
+if (grana){
+    console.log('vazei')
+}else{
+    console.log('nao fi');
+}
